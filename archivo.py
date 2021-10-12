86747f7a8d801c6f946dfb439c06e8d645f826b8
ggv8 <gabriel.go.v8@gmail.com>
2021-10-12 16:50:26 -0600

    Procesamiento y Archivos
    Funciones desarrolladas y documentadas para procesar registro de paquetes y su almacenamiento en binario

--- archivo.py
+++ archivo.py
@@ -4,5 +4,42 @@
 # Versión: 3.9.6
 
 ##############################################################
+#####              Importación de Librerías              #####
+##############################################################
+
+import pickle
+
+##############################################################
 #####              Definición de Funciones               #####
 ##############################################################
+
+def buscarBD():
+    """
+    Función:    Verifica si existe Base de Datos previa
+    Entradas:   N/A
+    Salidas:    Retorna contenidos BD. Si no, pide datos para crear BD
+    """
+    try:
+        print("\n" + "Cargando base de datos...".center(50, " "))
+        file = open('PaquetesBD', 'rb')
+        contenido = pickle.load(file)
+        file.close()
+        return contenido
+    except EOFError:            # Restricción: Base de Datos nula
+        print("\n" + "Error: Base de datos se encuentra vacía".center(50, " "))
+    except FileNotFoundError:   # Restricción: BD no encontrada o no existe
+        print("\n" + "No existe base de datos. Generando base de datos nueva...".center(50, " "))
+    return {}   # En caso de error, genera Base de Datos nueva
+
+def guardarBD(pregistro):
+    """
+    Función:    Guarda cambios realizados en archivo
+    Entradas:   pregistro (list) - Lista de datos tras cambios
+    Salidas:    Imprime mensaje confirmando que se han guardado cambios
+    """
+    file = open('PaquetesBD', 'wb')
+    pickle.dump(pregistro, file)
+    file.close()
+    print("\n" + "Base de datos actualizada.".center(50, " ") + "\n")
+    input("Continuar <ENTER>".center(50," "))
+    return
--- funciones.py
+++ funciones.py
@@ -6,8 +6,95 @@
 ##############################################################
 #####              Importación de Librerías              #####
 ##############################################################
+
 from wish import *
+
 ##############################################################
 #####              Definición de Funciones               #####
 ##############################################################
-print(insistirNombreSucursal())
+
+def confirmarOpcion(ptitulo, pmensaje):
+    """
+    Función:    Pide confirmación antes de realizar una tarea
+    Entradas:   ptitulo, pmensaje (str) - Título y contenido de opcion
+    Salidas:    Retorna True (Aceptar), False (Cancelar)
+    """
+    while True:
+        print("\n" + f"{ptitulo}".center(50,"=") + "\n")
+        print(pmensaje.center(50," "))
+        entrada = input("\n" + "<A> Aceptar  <C> Cancelar".center(49," "))
+        if entrada == "A":
+            return True
+        elif entrada == "C":
+            return False
+        imprimirError("Confirme o cancele con A o C") # Restricción: Entrada != B o C
+
+def ingresarPaquete(pregistro):
+    """
+    Función:    Submenú de entradas requeridas para registrar datos
+    Entradas:   pregistro (dict) - Diccionario de paquetes
+    Salidas:    Retorna registro de datos
+    """
+    while True:
+        print("\n" + " Registro de Datos ".center(50,"=") + "\n")
+        numero = validarPaquete()
+        if revisarPaquete(numero, pregistro):   # Restriccion: Paquete repetido
+            imprimirError("No se puede repetir número de paquete")
+            continue
+        telefono, sucursal = validarTelefono(), insistirNombreSucursal()
+        estado, dias = opcionInsistirEstadoPaquete(), 10
+        if estado != '1':   # Si estado es 2 o 3, cambia dias hábiles a 0
+            dias = 0
+        pregistro[numero] = [telefono, sucursal, dias, estado] # Crea paquete nuevo
+        if not confirmarOpcion("", "¿Desea ingresar más paquetes?"):
+            break   # Cierra submenu
+    return pregistro
+
+def actualizarPaquete(pregistro):
+    """
+    Función:    Submenú de entradas requeridas para actualizar datos
+    Entradas:   pregistro (dict) - Diccionario de paquetes
+    Salidas:    Retorna registro de datos
+    """
+    while True:
+        print("\n" + " Modificación de Datos ".center(50,"=") + "\n")
+        numero = validarPaquete()
+        if not revisarPaquete(numero, pregistro): # Restriccion: Paquete no registrado
+            imprimirError("No se encuentra registrado el paquete")
+            continue
+        estado = opcionInsistirEstadoPaquete()
+        if estado == pregistro[numero][-1]:     # Restricción: Estado ya registrado
+            imprimirError("El paquete ya tiene ese estado registrado")
+            continue
+        if confirmarOpcion(" ADVERTENCIA ", "¿Seguro que desea realizar este cambio?"):
+            dias = 0
+            if estado == '1':   # Si estado es 1, cambia dias habiles a 5
+                dias = 5
+            pregistro[numero][-2] = dias    # Actualizan dias habiles y estado
+            pregistro[numero][-1] = estado
+        if not confirmarOpcion("", "¿Desea modificar más paquetes?"):
+            break
+    return pregistro
+
+def eliminarPaquete(pregistro):
+    """
+    Función:    Submenú de entradas requeridas para eliminar datos
+    Entradas:   pregistro (dict) - Diccionario de paquetes
+    Salidas:    Retorna registro de datos
+    """
+    while True:
+        print("\n" + " Eliminación de Datos ".center(50,"=") + "\n")
+        numero = validarPaquete()
+        if not revisarPaquete(numero, pregistro):   # Restricción: Paquete no registrado
+            imprimirError("No se encuentra registrado el paquete")
+            continue
+        if confirmarOpcion(" ADVERTENCIA ", "¿Seguro que desea eliminar este paquete?"):
+            del pregistro[numero]
+        if not confirmarOpcion("", "¿Desea eliminar más paquetes?"):
+            break
+    return pregistro
+
+
+print(ingresarPaquete(x))
+print(actualizarPaquete(x))
+print(eliminarPaquete(x))
