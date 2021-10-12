605ace15f09f71d21269198c3404bb9eb71dd1f5
ggv8 <gabriel.go.v8@gmail.com>
2021-10-12 11:18:03 -0600

    Archivos py
    Creación de archivos iniciales para el laboratorio

---
+++ funciones.py
@@ -0,0 +1,8 @@
+# Elaborador por: Camilo Sánchez Rodríguez, Gabriel Gomez Vega
+# Fecha de creación: 12/10/2021, 11:16 AM
+# Última edición: XX/10/2021, XX:XX XX
+# Versión: 3.9.6
+
+##############################################################
+#####              Definición de Funciones               #####
+##############################################################
